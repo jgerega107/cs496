f1dd010cf41ebf7719b47c8cd39e9d3a60f0790f
jgerega107 <jakegerega107@gmail.com>
2021-02-11 20:32:12 -0500

    Initial commit

---
+++ 2-11.ml
@@ -0,0 +1,47 @@
+let rec fact n = 
+	match n with
+	| 0 -> 1
+	| m -> m * fact (m-1)
+
+let rec mem e l =
+	match l with
+	| [] -> false
+	| h::t -> if h=e then true else mem e t
+
+let rec rev l =
+	match l with
+	| [] -> []
+	| h::t -> rev t @ [h]
+
+	(*remember that the @ sign is append and :: is cons*)
+
+let rec stutter l =
+	match l with
+	| [] -> []
+	| h::t -> h::h::stutter t
+
+let has_size_at_least_two l =
+	match l with
+	| _::_::_ -> true
+	| _ -> false
+
+let rec succl l =
+	match l with
+	| [] -> []
+	| h::t -> h+1::succl t
+
+let rec greater_than_zero l =
+	match l with
+	| [] -> []
+	| h::t -> if h>0 then h :: greater_than_zero t else greater_than_zero t
+
+let rec andl l =
+	match l with
+	| [] -> true
+	| h::t -> h && andl t
+
+let rec map f l =
+	match l with
+	| [] -> []
+	| h::t -> f h :: map f t
+
